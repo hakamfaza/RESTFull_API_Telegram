2d202a3cb4c09e93b683a89e7e24327874125fb0
hakamfaza <hakamfaza@gmail.com>
2022-06-03 22:15:02 +0700

    update socket

--- src/socket/index.js
+++ src/socket/index.js
@@ -1,5 +1,15 @@
 module.exports = (io, socket) => {
-  socket.io('ping', (data) => {
+  socket.on('ping', (data) => {
     socket.emit('ping-response', data);
+  });
+  socket.on('join-room', (data) => {
+    // Receiver
+    const {
+      id, email, password, usernmae,
+    } = data;
+    socket.join(id);
+  });
+  socket.on('send-message', (data) => {
+    // io.emit('send-message-response', message);
   });
 };
