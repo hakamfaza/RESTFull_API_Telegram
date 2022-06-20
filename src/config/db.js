786103f1e6084f8a3ee42a47c4fb76ac28c62aa8
hakamfaza <hakamfaza@gmail.com>
2022-06-20 19:50:14 +0700

    make it better

--- src/config/db.js
+++ src/config/db.js
@@ -1,14 +1,30 @@
 const { Pool } = require('pg');
 const {
-  DB_HOST, DB_USER, DB_PASSWORD, DB_NAME, DB_PORT,
+  DB_HOST, DB_USER, DB_PASSWORD, DB_NAME, DB_PORT, NODE_ENV,
 } = require('../utils/env');
 
-const db = new Pool({
+const config = {
   host: DB_HOST,
   user: DB_USER,
   password: DB_PASSWORD,
   database: DB_NAME,
   port: DB_PORT,
+};
+
+if (NODE_ENV === 'production') {
+  config.ssl = {
+    rejectUnauthorized: false,
+  };
+}
+
+const db = new Pool(config);
+
+db.connect((err) => {
+  if (err) {
+    console.log(err.message);
+    process.exit(1);
+  }
+  console.log('Database connected successfully');
 });
 
 module.exports = db;
--- src/utils/env.js
+++ src/utils/env.js
@@ -5,6 +5,7 @@ module.exports = {
 
   DB_HOST: process.env.DB_HOST,
   DB_USER: process.env.DB_USER,
+  NODE_ENV: process.env.NODE_ENV,
   DB_PASSWORD: process.env.DB_PASSWORD,
   DB_NAME: process.env.DB_NAME,
   DB_PORT: process.env.DB_PORT,
